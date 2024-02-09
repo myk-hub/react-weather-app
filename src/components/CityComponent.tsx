544f18e4818b4ef7606860d0bb5f9372cb03322d
Mykhailo Hubal <n7username7@gmail.com>
2024-02-09 20:24:57 +0200

    update search city weather component ui

--- src/components/CityComponent.tsx
+++ src/components/CityComponent.tsx
@@ -1,33 +1,14 @@
 import React from "react";
 import styled from "styled-components";
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import LocationSearchingIcon from '@mui/icons-material/LocationSearching';
 
 const SearchBox = styled.form`
   display: flex;
   flex-direction: row;
   justify-content: space-evenly;
   margin: 20px;
-  border: black solid 1px;
-  border-radius: 2px;
-
-  & input {
-    padding: 10px;
-    font-size: 14px;
-    border: none;
-    outline: none;
-    font-family: Montserrat;
-    font-weight: bold;
-  }
-  & button {
-    background-color: black;
-    font-size: 14px;
-    padding: 0 10px;
-    color: white;
-    border: none;
-    outline: none;
-    cursor: pointer;
-    font-family: Montserrat;
-    font-weight: bold;
-  }
 `;
 
 const ChooseCityLabel = styled.span`
@@ -51,11 +32,14 @@ const CityComponent = ({ updateCity, fetchWeather }: {
       <WelcomeWeatherLogo src={"/react-weather-app/icons/perfect-day.svg"} />
       <ChooseCityLabel>Find Weather of your city</ChooseCityLabel>
       <SearchBox onSubmit={fetchWeather}>
-        <input
-          onChange={(e) => updateCity(e.target.value)}
-          placeholder="City"
-        />
-        <button type={"submit"}>Search</button>
+        <TextField 
+            id="outlined-basic"
+            label="City"
+            variant="standard"
+            placeholder="Enter your city"
+            onChange={(e: any) => updateCity(e.target.value)}
+          />
+        <Button startIcon={<LocationSearchingIcon />} type={"submit"}>Search</Button>
       </SearchBox>
     </>
   );
